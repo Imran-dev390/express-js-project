fc305e91154d3f53c434d6a7b403d3610a206e5c
Imran-dev390 <mi2506493@gmail.com>
2025-05-05 04:26:33 +0500

    final

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 
 const connectionDb = async ()=>{
     try{
-        await mongoose.connect(process.env.MONGO_URI);
+          await mongoose.connect(process.env.MONGO_URI,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
          console.log("Connected to Db");
     } catch(err){
         console.log("Connection Error",err.message)
