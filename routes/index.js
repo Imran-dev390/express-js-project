265759d7f4608b03bb48ba15c6f90e3fbc9edaa0
Imran-dev390 <mi2506493@gmail.com>
2025-05-05 07:22:41 +0500

    lost

--- routes/index.js
+++ routes/index.js
@@ -268,7 +268,7 @@ await user.save();
 
 
 
-router.post('/createpost',isLoggedIn, upload.single('postimage'), async (req, res) => {
+router.post('/createpost',upload.single('postimage'), async (req, res) => {
   try {
     const imageBuffer = req.file.buffer;
     const mimeType = req.file.mimetype;
